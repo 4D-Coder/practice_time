edac1872d9aad4610908b5d195a4e11bd2d2767a
Antonio King Hunt <tesseractcode@icloud.com>
2023-08-29 13:39:55 -0600

    Completed: 39mins

---
+++ hackerrank/staircase.rb
@@ -0,0 +1,21 @@
+
+def staircase(n)
+  collection = ""
+  counter = n
+  # Take in the integer and print to the termanal n times
+  n.times do
+      collection.concat("#")
+      justified = collection.rjust(n)
+
+      p justified
+  end
+end
+
+staircase(6)
+# =>
+# "     #"
+# "    ##"
+# "   ###"
+# "  ####"
+# " #####"
+# "######"
