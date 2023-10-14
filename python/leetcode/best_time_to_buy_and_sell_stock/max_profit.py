9c2275993c134b7da37fa3c968f456f1dc7a031d
Antonio King Hunt <tesseractcode@icloud.com>
2023-10-14 15:10:36 -0600

    Fix: removed notes for merge

--- python/leetcode/best_time_to_buy_and_sell_stock/max_profit.py
+++ python/leetcode/best_time_to_buy_and_sell_stock/max_profit.py
@@ -2,18 +2,18 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:  # Check if the prices list is empty
+        if not prices:
             return 0
 
-        min_price = prices[0]  # Initialize the minimum price
-        max_profit = 0  # Initialize the maximum profit
-        
+        min_price = prices[0]
+        max_profit = 0
+
         for price in prices:
             if price < min_price:
-                min_price = price  # Update the minimum price if a lower price is encountered
+                min_price = price
             else:
                 profit = price - min_price
                 if profit > max_profit:
-                    max_profit = profit  # Update the maximum profit if a better profit is found
+                    max_profit = profit 
 
         return max_profit
