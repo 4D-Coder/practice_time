22974066f1bf8d648e1046c1ac636a6d16e2f2b4
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-10-17 11:25:03 -0600

    Clean: remove notes for merge

--- python/leetcode/valid_palindrome/valid_palindrome.py
+++ python/leetcode/valid_palindrome/valid_palindrome.py
@@ -8,8 +8,6 @@ class Solution:
     return comparison[0] == comparison[1]
 
   def create_comparison(self, text) -> str:
-    # uses the string module to build a dictionary of all bit ID's as keys,
-    # assigning them to None as values
     translator = str.maketrans('', '', string.punctuation)
     scrubbed = text.translate(translator)
     joined = re.sub(' ', '', scrubbed)
