6ff05d7f6fff9bad148bcf53a524577b1a16e7b9
Antonio King Hunt <tesseractcode@icloud.com>
2023-10-21 11:41:34 -0600

    Add: testing by converting reversed tree back into list

--- python/leetcode/invert_binary_tree/tests/test_invert_binary_tree.py
+++ python/leetcode/invert_binary_tree/tests/test_invert_binary_tree.py
@@ -1,32 +1,36 @@
 import unittest
 import ipdb
 from invert_binary_tree import Solution, TreeNode
+import json
 
 class InvertTreeTestCase(unittest.TestCase):
   def test_input_1(self):
     solution = Solution()
-    sorted_tree = solution.toBinaryTree([4,2,7,1,3,6,9])
+    input = solution.toBinaryTree([4,2,7,1,3,6,9])
+    inverted = solution.invertTree(input)
 
-    expected = solution.toBinaryTree([4,7,2,9,6,3,1])
-    result = solution.invertTree(sorted_tree)
+    expected = [4,7,2,9,6,3,1]
+    result = solution.toList(inverted)
 
     self.assertEqual(result, expected)
 
   def test_input_2(self):
     solution = Solution()
-    sorted_tree = solution.toBinaryTree([2,1,3])
+    input = solution.toBinaryTree([2,1,3])
+    inverted = solution.invertTree(input)
 
-    expected = solution.toBinaryTree([2,3,1])
-    result = solution.invertTree(sorted_tree)
+    expected = [2,3,1]
+    result = solution.toList(inverted)
 
     self.assertEqual(result, expected)
 
   def test_input_3(self):
     solution = Solution()
-    sorted_tree = solution.toBinaryTree([])
+    input = solution.toBinaryTree([])
+    inverted = solution.invertTree(input)
 
-    expected = solution.toBinaryTree([])
-    result = solution.invertTree(sorted_tree)
+    expected = []
+    result = solution.toList(inverted)
 
     self.assertEqual(result, expected)
 
